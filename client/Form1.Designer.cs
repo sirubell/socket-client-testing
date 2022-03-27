20cfa390d0cfed749ce939aa9c0f2e229dc66eee
黃冠穎 <gary20011110@gmail.com>
2022-03-27 13:04:46 +0800

    update

--- client/Form1.Designer.cs
+++ client/Form1.Designer.cs
@@ -28,53 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.player = new System.Windows.Forms.PictureBox();
-            this.block1 = new System.Windows.Forms.PictureBox();
-            this.block2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.block1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.block2)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // player
+            // pictureBox1
             // 
-            this.player.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.player.BackColor = System.Drawing.Color.Lime;
-            this.player.Location = new System.Drawing.Point(418, 95);
-            this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(60, 90);
-            this.player.TabIndex = 7;
-            this.player.TabStop = false;
-            // 
-            // block1
-            // 
-            this.block1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.block1.Location = new System.Drawing.Point(134, 568);
-            this.block1.Name = "block1";
-            this.block1.Size = new System.Drawing.Size(300, 30);
-            this.block1.TabIndex = 8;
-            this.block1.TabStop = false;
-            // 
-            // block2
-            // 
-            this.block2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.block2.Location = new System.Drawing.Point(554, 376);
-            this.block2.Name = "block2";
-            this.block2.Size = new System.Drawing.Size(300, 30);
-            this.block2.TabIndex = 8;
-            this.block2.TabStop = false;
+            this.pictureBox1.Location = new System.Drawing.Point(263, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 75);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // formgame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(918, 1050);
-            this.Controls.Add(this.block2);
-            this.Controls.Add(this.block1);
-            this.Controls.Add(this.player);
+            this.ClientSize = new System.Drawing.Size(778, 1144);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
@@ -84,16 +56,13 @@
             this.Text = "小朋友下樓梯 Remake";
             this.Closing += new System.ComponentModel.CancelEventHandler(this.Form_Closing);
             this.Load += new System.EventHandler(this.Form_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.block1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.block2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private PictureBox player;
-        private PictureBox block1;
-        private PictureBox block2;
+
+        private PictureBox pictureBox1;
     }
 }
